dd910c8dbc93080a05b33509ba64c292898fc384
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-09-15 10:25:03 +0500

    full configure added

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const connectToDatabase = require("./database/db");
+const logger = require("./middleware/logger");
+const apps = require("./routes/apps");
 require("dotenv").config();
+
 const app = express();
 
-// taking port from env file
-const PORT = process.env.PORT;
+// connect to the db
+connectToDatabase();
 
 // middleware
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
+// routes
+app.use("/apps", apps);
+
+// taking port from env file
+const PORT = process.env.PORT;
 
 // listening port
 app.listen(PORT, () => {
