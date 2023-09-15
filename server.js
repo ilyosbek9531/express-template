ff3112698f83fe621ec39115335fc32efa5efe02
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-09-15 09:35:28 +0500

    configure

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 require("dotenv").config();
 const app = express();
 
 // taking port from env file
 const PORT = process.env.PORT;
+
+// middleware
+app.use(morgan("dev"));
 
 // listening port
 app.listen(PORT, () => {
